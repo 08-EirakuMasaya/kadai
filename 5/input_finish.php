9e22fc977ff3aec9acb328d5317de50891687b7e
08-EirakuMasaya <amsivee@gmail.com>
2015-09-05 13:47:20 +0900

    修正

--- 5/input_finish.php
+++ 5/input_finish.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $_SESSION['shohin_id'] = 1;
-$input_data = $_SESSION["name"];
+$input_data = $_SESSION["name"] . "," . $_SESSION["furigana"] . "," . $_SESSION["email"] . "," . $_SESSION["sex"] . "," . $_SESSION["age"];
 $file = fopen("./data/data.txt","a");	// ファイル読み込み aは引数（追加書き込み、appendと一緒）
 flock($file, LOCK_EX);			// ファイルロック
 fputs($file, $input_data . PHP_EOL);
