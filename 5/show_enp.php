33b2a52778b9bc985b3a24ad5fa0efbf93cd1ab1
08-EirakuMasaya <amsivee@gmail.com>
2015-09-06 13:47:11 +0900

    表組みの追加

--- 5/show_enp.php
+++ 5/show_enp.php
@@ -1,10 +1,22 @@
 <?php
 
 $file = "./data/question.csv";
-if ( ( $handle = fopen ( $file, "r" ) ) !== FALSE ) {
+if ( ( $handle = fopen ( $file, "r" ) ) != FALSE ) {
     echo "<table>\n";
-    while ( ( $data = fgetcsv ( $handle, 1000, ",", '"' ) ) !== FALSE ) {
-        echo "\t<tr>\n";
+    while ( ( $data = fgetcsv ( $handle, 1000, ",", '"' ) ) != FALSE ) {
+ /*{文字列長}（省略可能（PHP5から））
+取得する文字列の最大値。
+【省略値】制限なし（但し若干動作が遅くなる）
+
+{デミリタ文字}（省略可能）
+フィールドのデリミタ（区切文字）の設定。
+【省略値】カンマ
+
+{フィールド囲い子文字}（省略可能）
+フィールド囲い子文字の設定。
+【省略値】ダブルクォート
+*/
+        echo "\t<tr>\n"; // /tは水平タブ（縦位置を揃えるために右方向にタグをずらす）
         for ( $i = 0; $i < count( $data ); $i++ ) {
             echo "\t\t<td>{$data[$i]}</td>\n";
         }
